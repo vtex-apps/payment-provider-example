15f116b421c50023091f5ba83efb01b4f1fb3a2a
Mateus Ribeiro <mateus.ribeiro@vtex.com>
2026-02-10 16:57:09 -0300

    Generate first version of the mock connector for UCP

--- node/connector.ts
+++ node/connector.ts
@@ -1,6 +1,7 @@
 import {
   AuthorizationRequest,
   AuthorizationResponse,
+  Authorizations,
   CancellationRequest,
   CancellationResponse,
   Cancellations,
@@ -12,89 +13,47 @@ import {
   SettlementResponse,
   Settlements,
 } from '@vtex/payment-provider'
-import { VBase } from '@vtex/api'
 
 import { randomString } from './utils'
-import { executeAuthorization } from './flow'
 
-const authorizationsBucket = 'authorizations'
-const persistAuthorizationResponse = async (
-  vbase: VBase,
-  resp: AuthorizationResponse
-) => vbase.saveJSON(authorizationsBucket, resp.paymentId, resp)
-
-const getPersistedAuthorizationResponse = async (
-  vbase: VBase,
-  req: AuthorizationRequest
-) =>
-  vbase.getJSON<AuthorizationResponse | undefined>(
-    authorizationsBucket,
-    req.paymentId,
-    true
-  )
-
-export default class TestSuiteApprover extends PaymentProvider {
-  // This class needs modifications to pass the test suit.
-  // Refer to https://help.vtex.com/en/tutorial/payment-provider-protocol#4-testing
-  // in order to learn about the protocol and make the according changes.
-
-  private async saveAndRetry(
-    req: AuthorizationRequest,
-    resp: AuthorizationResponse
-  ) {
-    await persistAuthorizationResponse(this.context.clients.vbase, resp)
-    this.callback(req, resp)
-  }
+export default class UcpPaymentProviderMock extends PaymentProvider {
 
   public async authorize(
     authorization: AuthorizationRequest
   ): Promise<AuthorizationResponse> {
-    if (this.isTestSuite) {
-      const persistedResponse = await getPersistedAuthorizationResponse(
-        this.context.clients.vbase,
-        authorization
-      )
-
-      if (persistedResponse !== undefined && persistedResponse !== null) {
-        return persistedResponse
-      }
-
-      return executeAuthorization(authorization, response =>
-        this.saveAndRetry(authorization, response)
-      )
-    }
-
-    throw new Error('Not implemented')
+    return Authorizations.approve(authorization, {
+      authorizationId: randomString(),
+      nsu: randomString(),
+      tid: randomString(),
+    })
   }
 
   public async cancel(
     cancellation: CancellationRequest
   ): Promise<CancellationResponse> {
-    if (this.isTestSuite) {
-      return Cancellations.approve(cancellation, {
-        cancellationId: randomString(),
-      })
-    }
-
-    throw new Error('Not implemented')
+    return Cancellations.approve(cancellation, {
+      cancellationId: `${randomString()}`,
+      code: 'success',
+      message: 'Payment has been canceled.',
+    })
   }
 
   public async refund(refund: RefundRequest): Promise<RefundResponse> {
-    if (this.isTestSuite) {
-      return Refunds.deny(refund)
-    }
-
-    throw new Error('Not implemented')
+    return Refunds.approve(refund, {
+      refundId: `${randomString()}`,
+      code: 'success',
+      message: 'Payment has been refunded',
+    })
   }
 
   public async settle(
     settlement: SettlementRequest
   ): Promise<SettlementResponse> {
-    if (this.isTestSuite) {
-      return Settlements.deny(settlement)
-    }
-
-    throw new Error('Not implemented')
+    return Settlements.approve(settlement, {
+      settleId: randomString(),
+      code: 'success',
+      message: 'Payment has been captured',
+    })
   }
 
   public inbound: undefined
